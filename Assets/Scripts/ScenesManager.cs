1730e9c662bf4323a0095a974d913ed5f37ca71c
Ivaylo Ivanov <45109015+IIvanov21@users.noreply.github.com>
2023-02-03 10:59:06 +0000

    We have created a Scenes Manager script

--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Scenes
     {
+        bootUp,//Reference for the starting credits scene
+        title, //Main menu and title
+        waveOne,
+        waveTwo,
+        waveThree,
+        waveBoss,//Final boss level
+        gameOver,//Load into when the player wins or dies
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("SampleScene");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        //Only works when we fully build out our game
+        Application.Quit();
     }
 }
